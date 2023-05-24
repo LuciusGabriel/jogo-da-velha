9e4c0fa4247697ae9ac1bc2ec63ae03f7841bc26
LuciusGabriel <lucius_gabriel@hotmail.com>
2023-05-23 22:22:25 -0300

    Função reset

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 // Dados Iniciais
-let quadro = {
+let square = {
     a1:'', a2:'', a3:'',
     b1:'', b2:'', b3:'',
     c1:'', c2:'', c3:'',
-    
 }
+let player = ''
+let warning = ''
+let playing = false
 
 // Eventos
+document.querySelector('.reset').addEventListener('click', reset)
 
 // Funções
+function reset(){
+    warning = ''
+
+    let random = Math.floor(Math.random() * 2)
+
+    if(random === 0){
+        player = 'x'
+    }else{
+        player = 'o'
+    }
+
+    for(let i in square){
+        square[i] = ''
+    }
+
+    playing = true
+}
